88fd5f9d19129e29239e1029ed67498a653099d0
novinary <novina.dev@gmail.com>
2019-06-10 15:37:09 +0100

    Implemented queue data structure

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,28 @@
+'''
+- A queue is nothing more than a list which is limited 
+to only allow data to be inserted from one end, and 
+retrieved from the other. 
+
+- Known as FIFO data structure - First In - First Out!
+- Data is pushed in at the Back end which is called enqueue
+- Data is popped out at the Front end which is called dequeue
+- There is no ability to insert or retrieve from any other location
+  in the list 
+'''
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = []
 
   def enqueue(self, item):
-    pass
+    return self.storage.append(item)  # Push/append an item to the end of queue
   
   def dequeue(self):
-    pass
+    if len(self.storage):
+      return self.storage.pop(0)     # Pop an item to the other end of the queue(first item in the list)
 
   def len(self):
-    pass
+    return len(self.storage)
